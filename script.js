ba0c77b3ee2cd3205726138da54db3e0d8192bc2
Kevin David <corraleskevin276@gmail.com>
2025-10-26 14:08:56 -0500

    Agregando contacto y nuevos script

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Al hacer clic en el nombre del negocio
 document.getElementById("nombre-negocio").addEventListener("click", function() {
-  alert("Mira ve! la mejor tienda de Cali ois!");
+  alert("Mira ve! la mejor tienda de Cali ois!😎");
 });
+
+// Funcionalidad del botón "Agregar al carrito"
+const botones = document.querySelectorAll(".btn-agregar");
+botones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    alert("Tu producto fue agregado al carrito");
+  });
+});
+
+// Formulario de contacto
+const formulario = document.getElementById("form-contacto");
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que se recargue la página
+  alert("Gracias por contactarnos");
+  formulario.reset(); // Limpia los campos después del mensaje
+});
+
  
